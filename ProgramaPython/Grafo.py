e795fdc6acf27bed283156341cad6f20aa58cb86
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-05-21 22:27:43 -0600

    agregar partes a la parte de python

--- ProgramaPython/Grafo.py
+++ ProgramaPython/Grafo.py
@@ -15,27 +15,34 @@ with open("guategrafo.txt") as file_object:
 
 fin = False
 while not fin:
-    opcion = input("\nIngrese la opcion a realizar:\n1. Mostrar ruta mas corta de una ciudad a otra\n2. Centro del grafo\n3. Agregar interrupcion de trafico\n4. Establecer nueva conexion\n5. Salir")
+    opcion = input("\nIngrese la opcion a realizar:\n1. Mostrar ruta mas corta de una ciudad a otra\n2. Centro del grafo\n3. Agregar interrupcion de trafico\n4. Establecer nueva conexion\n5. Imprimir el grafo\n6. Salir\n")
     if (opcion=="1"):
-        origen = input("Ingresar la ciudad de origen")
-        destino = input("Ingresar la ciudad de destino")
-        
+        origen = input("Ingresar la ciudad de origen\n")
+        destino = input("Ingresar la ciudad de destino\n")
+        pre,_=nx.floyd_warshall_predecessor_and_distance(G)
+        print("Camino mas corto de "+origen+" a "+destino+":\n")
+        print(nx.reconstruct_path(origen, destino, pre))
             
     elif (opcion =="2"):
         regresar=0
     elif (opcion =="3"):
-        origen = input("Ingresar la ciudad de origen")
-        destino = input("Ingresar la ciudad de destino")
+        origen = input("Ingresar la ciudad de origen\n")
+        destino = input("Ingresar la ciudad de destino\n")
         G.remove_edge(origen,destino)
         print("\nAccion realizada")
     elif (opcion =="4"):
-        origen = input("Ingresar la ciudad de origen")
-        destino = input("Ingresar la ciudad de destino")
-        distancia = int(input("Ingresar la ciudad distancia"))
-        G.add_edge(origen,destino,distancia)
+        origen = input("Ingresar la ciudad de origen\n")
+        destino = input("Ingresar la ciudad de destino\n")
+        distancia = int(input("Ingresar la ciudad distancia\n"))
+        G.add_edge(origen,destino,weight=distancia)
         
         print("\nAccion realizada")
-    elif (opcion =="5"):
+    elif (opcion=="5"):
+        print("Nodos:\n")
+        print(G.nodes)
+        print("Aristas:\n")
+        print(G.edges)
+    elif (opcion =="6"):
         print("Saliendo del programa")
         fin = True
     else:
